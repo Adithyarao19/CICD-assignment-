0ba56aa26699f15a10613961b1821c44737d0772
Adithyarao19 <adikudige619gmail.com>
2025-09-15 21:14:26 +0530

    fixed test job

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,22 +3,3 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.app.repository_url
 }
 
-output "cloudfront_domain" {
-  description = "CloudFront distribution domain name"
-  value       = aws_cloudfront_distribution.app.domain_name
-}
-
-output "alb_dns" {
-  description = "ALB DNS name"
-  value       = aws_lb.main.dns_name
-}
-
-output "ecs_cluster_name" {
-  description = "ECS cluster name"
-  value       = aws_ecs_cluster.main.name
-}
-
-output "ecs_service_name" {
-  description = "ECS service name"
-  value       = aws_ecs_service.main.name
-}
